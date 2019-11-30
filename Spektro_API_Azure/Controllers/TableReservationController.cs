e36a8895ebc3496a727cfbb385acec7d41a0f615
Patrik Andó <andopatrik0@gmail.com>
2019-11-30 22:28:15 +0100

    POST added

--- Spektro_API_Azure/Controllers/TableReservationController.cs
+++ Spektro_API_Azure/Controllers/TableReservationController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
 using Spektro_API_Azure.Model;
+using Spektro_API_Azure.Service;
 
 namespace Spektro_API_Azure.Controllers
 {
@@ -20,11 +21,13 @@ namespace Spektro_API_Azure.Controllers
         [HttpGet]
         public List<ReservationModel> GetAllReservations()
         {
-            return new List<ReservationModel>{ };
+            ReservationModel r = new ReservationModel();
+            r.EmailAddress = "yo";
+            return new List<ReservationModel> { r };
         }
 
         // GET: api/TableReservation/Mark
-        [HttpGet("{name}", Name = "Get")]
+        [HttpGet("{name}", Name = "GetReservationByName")]
         public ReservationModel GetReservationByName(string name)
         {
             return new ReservationModel();
@@ -32,9 +35,36 @@ namespace Spektro_API_Azure.Controllers
 
         // POST: api/TableReservation
         [HttpPost]
-        public void Post([FromBody] ReservationModel input)
+        public ActionResult Post([FromBody] ReservationModel input)
         {
-            //TODO: Write values from input to DB.
+            string commandString = "INSERT INTO Reservations (FirstName, LastName, Email, PhoneNo, EmailNotification, SmsNotification, NoOfPeople , DateOfReservation)"+
+                                   "VALUES(@FirstName, @LastName, @Email, @PhoneNo, @EmailNoti, @SmsNoti, @NoOfPeople, @DateOfReservation)";
+            
+            using (SqlConnection connection = new SqlConnection(ConnectionString.GetConnectionString()))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(commandString, connection))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", input.FirstName);
+                    cmd.Parameters.AddWithValue("@LastName", input.LastName);
+                    cmd.Parameters.AddWithValue("@Email", input.EmailAddress);
+                    cmd.Parameters.AddWithValue("@PhoneNo", input.PhoneNumber);
+                    cmd.Parameters.AddWithValue("@EmailNoti", input.EmailNotifcation);
+                    cmd.Parameters.AddWithValue("@SmsNoti", input.SmsNotification);
+                    cmd.Parameters.AddWithValue("@NoOfPeople", input.NoOfPeople);
+                    cmd.Parameters.AddWithValue("@DateOfReservation", input.DateOfReservation);
+
+                    int effectedDBrows = cmd.ExecuteNonQuery();
+
+                    if (effectedDBrows > 0)
+                    {
+
+                        return Ok();
+                    }
+
+                    return Problem();
+                }
+            }
         }
 
         // PUT: api/TableReservation/5
