42773b367cf05f0911be5346bb69b88f549f5a1e
KongChristian <christianmadhan@gmail.com>
2019-12-01 15:40:05 +0100

    Login with JWT is set up

--- Spektro_API_Azure/Service/ConnectionString.cs
+++ Spektro_API_Azure/Service/ConnectionString.cs
@@ -4,9 +4,16 @@
     {
         private static string conString = "Server=tcp:3rdsemprogramming.database.windows.net,1433;Initial Catalog=trialdb;Persist Security Info=False;User ID=patr589d;Password=CookieMonsta88;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
+        private static string secretKey = "spektro_team_ando_eckmann_kolesar_witt_next_level_developers";
+
         public static string GetConnectionString() 
         {
             return conString;
+        }
+
+        public static string GetSecretKey()
+        {
+            return secretKey;
         }
     }
 }
