9fdeb26d0cabff37cb04cfd9b1de7f22e111779d
Patrik Kolesar <patr273e@edu.easj.dk>
2019-12-04 18:11:42 +0100

    customerpost

---
+++ Spektro_API_Azure/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Spektro_API_Azure.Model;
+using Spektro_API_Azure.Service;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Spektro_API_Azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        [HttpPost]
+        public ActionResult Post([FromBody] CustomerModel input)
+        {
+            string commandString = "INSERT INTO Users (UserRole, Email, Kodeord, FirstName, LastName, EmailNotification, SmsNotification, PhoneNo)" +
+                                   "VALUES(@UserRole, @Email, @Kodeord, @FirstName, @LastName, @EmailNotification, @SmsNotification, @PhoneNo)";
+
+
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString.GetConnectionString()))
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand(commandString, connection))
+                    {
+                        cmd.Parameters.AddWithValue("@UserRole", input.UserRole);
+                        cmd.Parameters.AddWithValue("@Email", input.Email);
+                        cmd.Parameters.AddWithValue("@Kodeord", input.Kodeord);
+                        cmd.Parameters.AddWithValue("@FirstName", input.FirstName);
+                        cmd.Parameters.AddWithValue("@LastName", input.LastName);
+                        cmd.Parameters.AddWithValue("@EmailNotification", input.EmailNotification);
+                        cmd.Parameters.AddWithValue("@SmsNotification", input.SmsNotification);
+                        cmd.Parameters.AddWithValue("@PhoneNo", input.PhoneNo);
+
+                        int effectedDBrows = cmd.ExecuteNonQuery();
+
+                        return Problem();
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                throw e;
+
+            }
+        }
+    }
+}
---
+++ Spektro_API_Azure/Model/CustomerModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Spektro_API_Azure.Model
+{
+    public class CustomerModel
+    {
+        private int id;
+        private string userModel;
+        private string email;
+        private string koreord;
+        private string firstName;
+        private string lastName;
+        private bool emailNotification;
+        private bool smsNotification;
+        private int phoneNo;
+    }
+
+    public CustomerModel(){}
+
+    public CustomerModel(int id, string userModel, string email, string kodeord, string firstName, string lastName, bool emailNotification, bool smsNotification, int phoneNo) {
+        this.id = id;
+        this.userModel=userModel;
+        this.email=email;
+        this.kodeord=kodeord;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.emailNotification=emailNotification;
+        this.smsNotification=smsNotification;
+        this.phoneNo=phoneNo;
+    }
+
+public int Id { get => id; set => id = value; }
+public string UserModel { get => userModel; set => userModel = value; }
+public string Email { get => email; set => email = value; }
+public string Kodeord { get => kodeord; set => kodeord = value; }
+public string FirstName { get => firstName; set => firstName = value; }
+public string LastName { get => lastName; set => lastName = value; }
+public bool EmailNotification { get => emailNotification; set => emailNotification = value; }
+public bool SmsNotification { get => smsNotification; set => smsNotification = value; }
+public int PhoneNo { get=>phoneNo; set =>phoneNo=value;}
+}
