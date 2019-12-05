0cade96ee13e1dfc492020162673ebe0e6409c2f
Patrik Andó <andopatrik0@gmail.com>
2019-12-05 17:04:02 +0100

    Correction & Extension on add new customer
    Syntax & logic error corrections
    Check  for data.length
    Check for existing email / phoneNo

--- Spektro_API_Azure/Controllers/CustomerController.cs
+++ Spektro_API_Azure/Controllers/CustomerController.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Spektro_API_Azure.Model;
 using Spektro_API_Azure.Service;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Spektro_API_Azure.Controllers
 {
@@ -17,37 +12,63 @@ namespace Spektro_API_Azure.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] CustomerModel input)
         {
-            string commandString = "INSERT INTO Users (UserRole, Email, Kodeord, FirstName, LastName, EmailNotification, SmsNotification, PhoneNo)" +
+            string commandStringSelect = "select * from Users where Email = @Email OR PhoneNo = @PhoneNo;";
+
+            string commandStringInsert = "INSERT INTO Users (UserRole, Email, Kodeord, FirstName, LastName, EmailNotification, SmsNotification, PhoneNo)" +
                                    "VALUES(@UserRole, @Email, @Kodeord, @FirstName, @LastName, @EmailNotification, @SmsNotification, @PhoneNo)";
 
+            if (input.UserRole.Length > 10 || input.Email.Length > 50 || input.Kodeord.Length > 60 || input.FirstName.Length > 30 || 
+                input.LastName.Length > 30 || input.PhoneNo.Length > 20 || input.EmailNotification.GetType() != typeof(bool) ||
+                input.SmsNotification.GetType() != typeof(bool))
 
+            {
+                return Conflict("Incorrect data format or length");
+            }
 
             try
             {
                 using (SqlConnection connection = new SqlConnection(ConnectionString.GetConnectionString()))
                 {
                     connection.Open();
-                    using (SqlCommand cmd = new SqlCommand(commandString, connection))
+
+                    using (SqlCommand checkCmd = new SqlCommand(commandStringSelect, connection)) 
                     {
-                        cmd.Parameters.AddWithValue("@UserRole", input.UserRole);
-                        cmd.Parameters.AddWithValue("@Email", input.Email);
-                        cmd.Parameters.AddWithValue("@Kodeord", input.Kodeord);
-                        cmd.Parameters.AddWithValue("@FirstName", input.FirstName);
-                        cmd.Parameters.AddWithValue("@LastName", input.LastName);
-                        cmd.Parameters.AddWithValue("@EmailNotification", input.EmailNotification);
-                        cmd.Parameters.AddWithValue("@SmsNotification", input.SmsNotification);
-                        cmd.Parameters.AddWithValue("@PhoneNo", input.PhoneNo);
+                        checkCmd.Parameters.AddWithValue("@Email", input.Email);
+                        checkCmd.Parameters.AddWithValue("@PhoneNo", input.PhoneNo);
 
-                        int effectedDBrows = cmd.ExecuteNonQuery();
+                        using (SqlDataReader reader = checkCmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                return Conflict("This email or phone number has already been used.");
+                            }
+                        }
 
-                        return Problem();
-                    }
+                        using (SqlCommand cmd = new SqlCommand(commandStringInsert, connection))
+                        {
+                            cmd.Parameters.AddWithValue("@UserRole", input.UserRole);
+                            cmd.Parameters.AddWithValue("@Email", input.Email);
+                            cmd.Parameters.AddWithValue("@Kodeord", input.Kodeord);
+                            cmd.Parameters.AddWithValue("@FirstName", input.FirstName);
+                            cmd.Parameters.AddWithValue("@LastName", input.LastName);
+                            cmd.Parameters.AddWithValue("@EmailNotification", input.EmailNotification);
+                            cmd.Parameters.AddWithValue("@SmsNotification", input.SmsNotification);
+                            cmd.Parameters.AddWithValue("@PhoneNo", input.PhoneNo);
+
+                            int effectedDBrows = cmd.ExecuteNonQuery();
+
+                            if (effectedDBrows > 0)
+                            {
+                                return Ok("User created");
+                            }
+                            return Conflict();
+                        }
+                    }                  
                 }
             }
             catch (SqlException e)
             {
                 throw e;
-
             }
         }
     }
--- Spektro_API_Azure/Model/CustomerModel.cs
+++ Spektro_API_Azure/Model/CustomerModel.cs
@@ -1,45 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-using System.Threading.Tasks;
-
-namespace Spektro_API_Azure.Model
+﻿namespace Spektro_API_Azure.Model
 {
     public class CustomerModel
     {
         private int id;
-        private string userModel;
+        private string userRole;
         private string email;
-        private string koreord;
+        private string kodeord;
         private string firstName;
         private string lastName;
         private bool emailNotification;
         private bool smsNotification;
-        private int phoneNo;
-    }
+        private string phoneNo;
 
-    public CustomerModel(){}
+        public CustomerModel() { }
 
-    public CustomerModel(int id, string userModel, string email, string kodeord, string firstName, string lastName, bool emailNotification, bool smsNotification, int phoneNo) {
-        this.id = id;
-        this.userModel=userModel;
-        this.email=email;
-        this.kodeord=kodeord;
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.emailNotification=emailNotification;
-        this.smsNotification=smsNotification;
-        this.phoneNo=phoneNo;
-    }
+        public CustomerModel(int id, string userRole, string email, string kodeord, string firstName, string lastName, bool emailNotification, bool smsNotification, string phoneNo)
+        {
+            this.id = id;
+            this.userRole = userRole;
+            this.email = email;
+            this.kodeord = kodeord;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailNotification = emailNotification;
+            this.smsNotification = smsNotification;
+            this.phoneNo = phoneNo;
+        }
 
-public int Id { get => id; set => id = value; }
-public string UserModel { get => userModel; set => userModel = value; }
-public string Email { get => email; set => email = value; }
-public string Kodeord { get => kodeord; set => kodeord = value; }
-public string FirstName { get => firstName; set => firstName = value; }
-public string LastName { get => lastName; set => lastName = value; }
-public bool EmailNotification { get => emailNotification; set => emailNotification = value; }
-public bool SmsNotification { get => smsNotification; set => smsNotification = value; }
-public int PhoneNo { get=>phoneNo; set =>phoneNo=value;}
+        public int Id { get => id; set => id = value; }
+        public string UserRole { get => userRole; set => userRole = value; }
+        public string Email { get => email; set => email = value; }
+        public string Kodeord { get => kodeord; set => kodeord = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public bool EmailNotification { get => emailNotification; set => emailNotification = value; }
+        public bool SmsNotification { get => smsNotification; set => smsNotification = value; }
+        public string PhoneNo { get => phoneNo; set => phoneNo = value; }
+    } 
 }
