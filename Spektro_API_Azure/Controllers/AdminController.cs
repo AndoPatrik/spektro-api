2dba98bda87598ec9f60b1bcdb66e95d460928a4
Patrik Kolesar <patr273e@edu.easj.dk>
2019-12-09 20:05:48 +0100

    AdminController added

---
+++ Spektro_API_Azure/Controllers/AdminController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Spektro_API_Azure.Model;
+using Spektro_API_Azure.Service;
+
+namespace Spektro_API_Azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        // GET: api/Admin
+        [HttpGet]
+        public IEnumerable<ReservationModel> GetAllReservations()
+        {
+            const string selectString = "select * from Reservations order by id";
+            using (SqlConnection databaseConnection = new SqlConnection(ConnectionString.GetConnectionString()))
+            {
+                databaseConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectString, databaseConnection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        List<ReservationModel> reservationList = new List<ReservationModel>();
+                        while (reader.Read())
+                        {
+                            ReservationModel reservation = ReadReservations(reader);
+                            reservationList.Add(reservation);
+                        }
+                        return reservationList;
+                    }
+                }
+            }
+        }
+
+
+        private static ReservationModel ReadReservations(IDataRecord reader)
+        {
+            
+            string firstname = reader.IsDBNull(0) ? null : reader.GetString(0);
+            string lastname = reader.IsDBNull(1) ? null : reader.GetString(1);
+            string emailadress = reader.IsDBNull(2) ? null : reader.GetString(2);
+            string phonenumber = reader.IsDBNull(3) ? null : reader.GetString(3);
+            bool emailnotification = reader.GetBoolean(4);
+            bool smsnotification = reader.GetBoolean(5);
+            int noofpeople = reader.GetInt32(6);
+            DateTime dateofreservation = reader.GetDateTime(7);
+           
+
+
+
+            ReservationModel reservation = new ReservationModel
+            {
+                FirstName = firstname,
+                LastName = lastname,
+                EmailAddress = emailadress,
+                PhoneNumber = phonenumber,
+                EmailNotifcation = emailnotification,
+                SmsNotification = smsnotification,
+                NoOfPeople = noofpeople,
+                DateOfReservation=dateofreservation
+            };
+            return reservation;
+        }
+
+        // GET: api/Admin/5
+        [Route("{id}")]
+        public ReservationModel GeCouponById(int id)
+        {
+            const string selectString = "select * from Reservations where id=@id";
+            using (SqlConnection databaseConnection = new SqlConnection(ConnectionString.GetConnectionString()))
+            {
+                databaseConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectString, databaseConnection))
+                {
+                    selectCommand.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        if (!reader.HasRows) { return null; }
+                        reader.Read(); // advance cursor to first row
+                        return ReadReservations(reader);
+                    }
+                }
+            }
+        }
+
+        // POST: api/Admin
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Admin/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
