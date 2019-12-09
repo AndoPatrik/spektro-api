57406e2c503c083358a945d5278da5dd3e059e82
Patrik Kolesar <patr273e@edu.easj.dk>
2019-12-09 19:44:45 +0100

    now

--- Spektro_API_Azure/Model/CouponModel.cs
+++ Spektro_API_Azure/Model/CouponModel.cs
@@ -7,5 +7,32 @@ namespace Spektro_API_Azure.Model
 {
     public class CouponModel
     {
+        private int id;
+        private string label;
+        private string description;
+        private int discount;
+        private DateTime dateOfIssue;
+        private DateTime dateOfExpiry;
+        private bool validity;
+
+        public CouponModel() { }
+        public CouponModel(int id, string label, string description, int discount, DateTime dateOfIssue, DateTime dateOfExpiry, bool validity)
+        {
+            this.id = id;
+            this.label = label;
+            this.description = description;
+            this.discount = discount;
+            this.dateOfIssue = dateOfIssue;
+            this.dateOfExpiry = dateOfExpiry;
+            this.validity = validity;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Label { get => label; set => label = value; }
+        public string Description { get => description; set => description = value; }
+        public int Discount { get => discount; set => discount = value; }
+        public DateTime DateOfIssue { get => dateOfIssue; set => dateOfIssue = value; }
+        public DateTime DateOfExpiry { get => dateOfExpiry; set => dateOfExpiry = value; }
+        public bool Validity { get => validity; set => validity = value; }
     }
 }
