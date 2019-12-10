e76e1d9c3b956b0179b2436e807082092022b8d5
AndoPatrik <andopatrik0@gmail.com>
2019-12-10 10:38:20 +0100

    Add Id to getall and getid + update started on admin/reservation

--- Spektro_API_Azure/Model/ReservationModel.cs
+++ Spektro_API_Azure/Model/ReservationModel.cs
@@ -12,8 +12,12 @@ namespace Spektro_API_Azure.Model
         private bool smsNotification;
         private int noOfPeople;
         private DateTime dateOfReservation;
+        private int id;
+
 
         public ReservationModel() { }
+
+
 
         public ReservationModel(string firstName, string lastName, string emailAddress, string phoneNumber, bool emailNotifcation, bool smsNotification, int noOfPeople, DateTime dateOfReservation)
         {
@@ -27,6 +31,19 @@ namespace Spektro_API_Azure.Model
             this.dateOfReservation = dateOfReservation;
         }
 
+        public ReservationModel(string firstName, string lastName, string emailAddress, string phoneNumber, bool emailNotifcation, bool smsNotification, int noOfPeople, DateTime dateOfReservation, int id)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.phoneNumber = phoneNumber;
+            this.emailNotifcation = emailNotifcation;
+            this.smsNotification = smsNotification;
+            this.noOfPeople = noOfPeople;
+            this.dateOfReservation = dateOfReservation;
+            this.id = id;
+        }
+
         public string FirstName { get => firstName; set => firstName = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string EmailAddress { get => emailAddress; set => emailAddress = value; }
@@ -35,5 +52,6 @@ namespace Spektro_API_Azure.Model
         public bool SmsNotification { get => smsNotification; set => smsNotification = value; }
         public int NoOfPeople { get => noOfPeople; set => noOfPeople = value; }
         public DateTime DateOfReservation { get => dateOfReservation; set => dateOfReservation = value; }
+        public int Id { get => id; set => id = value; }
     }
 }
