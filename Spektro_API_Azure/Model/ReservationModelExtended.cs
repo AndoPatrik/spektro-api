dd04b942235683832a12fb12a93d46e4ed9b7804
KongChristian <christianmadhan@gmail.com>
2019-12-10 10:49:32 +0100

    created extended reservattion model

---
+++ Spektro_API_Azure/Model/ReservationModelExtended.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Spektro_API_Azure.Model
+{
+    public class ReservationModelExtended
+    {
+        private string firstName;
+        private string lastName;
+        private string emailAddress;
+        private string phoneNumber;
+        private bool emailNotifcation;
+        private bool smsNotification;
+        private int noOfPeople;
+        private DateTime dateOfReservation;
+        private int Id;
+
+
+        public ReservationModelExtended() { }
+
+        public ReservationModelExtended(string firstName, string lastName, string emailAddress, string phoneNumber, bool emailNotifcation, bool smsNotification, int noOfPeople, DateTime dateOfReservation, int Id)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.emailAddress = emailAddress;
+            this.phoneNumber = phoneNumber;
+            this.emailNotifcation = emailNotifcation;
+            this.smsNotification = smsNotification;
+            this.noOfPeople = noOfPeople;
+            this.dateOfReservation = dateOfReservation;
+            this.Id = Id;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string EmailAddress { get => emailAddress; set => emailAddress = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public bool EmailNotifcation { get => emailNotifcation; set => emailNotifcation = value; }
+        public bool SmsNotification { get => smsNotification; set => smsNotification = value; }
+        public int NoOfPeople { get => noOfPeople; set => noOfPeople = value; }
+        public DateTime DateOfReservation { get => dateOfReservation; set => dateOfReservation = value; }
+        public int ID { get => Id; set => Id = value; }
+    }
+}
