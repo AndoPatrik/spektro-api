037661b4451e18fb1f142c2cbde07413d8b358d6
AndoPatrik <andopatrik0@gmail.com>
2019-12-13 03:08:39 +0100

    Scheduler system set up + bone of the service

---
+++ Spektro_API_Azure/Controllers/ReservationNotificationScheduler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spektro_API_Azure.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationNotificationScheduler : ControllerBase
+    {
+        private int _hour = 7;
+        private int _minute = 0;
+
+        [Route("NextTrigger")]
+        [HttpGet]
+        public ActionResult GetSetupInfo()
+        {
+
+            return Ok($"Notifications will be sent each day at {_hour}:{_minute}") ;
+        }
+
+        [Route("SetJob/h:{hour}/m:{minute}")]
+        [HttpGet]
+        public ActionResult ApplyOrUpdateReservationNotifications(int hour, int minute)
+        {
+            _hour = hour;
+            _minute = minute;
+
+            RecurringJob.AddOrUpdate(recurringJobId: "DailyReservationNotification", methodCall: () => Console.WriteLine("Call here the actual method"), Cron.Daily(_hour,_minute)); 
+            return Ok();
+        }
+
+
+
+        // POST: api/NotificationCenter
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+    }
+}
--- Spektro_API_Azure/Startup.cs
+++ Spektro_API_Azure/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication;
+using Hangfire;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Spektro_API_Azure.Service;
 
@@ -30,6 +23,9 @@ namespace Spektro_API_Azure
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHangfire(x => x.UseSqlServerStorage(ConnectionString.GetConnectionString()));
+            services.AddHangfireServer();
+
             // Security Key  from Service
             string securityKey = ConnectionString.GetSecretKey();
 
@@ -57,6 +53,10 @@ namespace Spektro_API_Azure
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseHangfireServer();
+            app.UseHangfireDashboard(pathMatch: "/hangfire");
+
+  
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
