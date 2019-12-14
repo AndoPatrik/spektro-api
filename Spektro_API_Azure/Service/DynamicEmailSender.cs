c41b6a452990653677a26609d019a2638e74bdee
dani337x <dani337x@edu.easj.dk>
2019-12-14 17:21:28 +0100

    Dynamic Email sender method has been created
    The method itself and placeholder HTML email bodies under the EmailAssets folder.

---
+++ Spektro_API_Azure/Service/DynamicEmailSender.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace Spektro_API_Azure.Service
+{
+    public class DynamicEmailSender
+    {
+        public static void SendCustomEmail(string firstname, string lastname, string email, DateTime date, string mailtype, string subject)
+        {
+            string mailContent = "";
+
+            string couponPath = "EmailAssets\\Coupon.html";
+            string forgottenPath = "EmailAssets\\Forgotten.html";
+            string promoPath = "EmailAssets\\Promo.html";
+            string registrationPath = "EmailAssets\\Registration.html";
+            string reservationPath = "EmailAssets\\Reservation.html";
+
+            // mailtypes: COUPON, RESERVATION, PROMO, REGISTRATION, FORGOTTEN
+            if (mailtype == "COUPON")
+            {
+                mailContent = @"Hello " + firstname + " " + lastname + ",<br>" + File.ReadAllText(couponPath);
+            }
+            else if (mailtype == "RESERVATION")
+            {
+                mailContent = @"Hello " + firstname + " " + lastname + ",<br>" + File.ReadAllText(reservationPath);
+            }
+            else if (mailtype == "PROMO")
+            {
+                mailContent = @"Hello " + firstname + " " + lastname + ",<br>" + File.ReadAllText(promoPath);
+            }
+            else if (mailtype == "REGISTRATION")
+            {
+                mailContent = @"Hello " + firstname + " " + lastname + ",<br>" + File.ReadAllText(registrationPath);
+            }
+            else if (mailtype == "FORGOTTEN")
+            {
+                mailContent = @"Hello " + firstname + " " + lastname + ",<br>" + File.ReadAllText(forgottenPath);
+            };
+
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+            MailMessage msg = null;
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.EnableSsl = true;
+
+            string emailSenderUserName = "spektro.noreply@gmail.com";
+            string emailSenderPw = "Spektro2019";
+            smtpClient.Credentials = new System.Net.NetworkCredential(emailSenderUserName, emailSenderPw);
+
+            try
+            {
+                msg = new MailMessage("spektro.noreply@gmail.com", email);
+                msg.Subject = subject;
+                msg.Body = mailContent;
+                msg.IsBodyHtml = true;
+                smtpClient.Send(msg);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (msg != null)
+                {
+                    msg.Dispose();
+                }
+            }
+
+        }
+    }
+}
